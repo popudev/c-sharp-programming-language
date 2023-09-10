d6e53315541a081f91609548b63c984ca5e12b9e
KidP2H <40386174+kidp2h@users.noreply.github.com>
2023-09-10 19:52:03 +0700

    Add files via upload

---
+++ Week1/Week1/Exercise12.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1
+{
+    internal class Exercise12 : BaseExercise
+    {
+		public bool isSquareNumber(int n)
+		{
+    		double result = Math.Sqrt(n);
+			return result%1 == 0;
+		}
+  		public void Run () {
+			int n = 0;
+			do {
+				Console.Write("Nhập số chính phương n: ");
+        		n = int.Parse(Console.ReadLine());
+			}while(!isSquareNumber(n));
+			Console.Write($"Bạn vừa nhập số chính phương là {n}");
+  		}
+		
+    }
+}
---
+++ Week1/Week1/Exercise13.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1
+{
+    internal class Exercise13 : BaseExercise
+    {
+  		public void Run () {
+			Console.Write("Nhập n: ");
+        	int n = int.Parse(Console.ReadLine());
+			int temp = n;
+			int count;
+	    	for(int i = 2; i <= temp; i++){
+        		count = 0;
+        		while(temp % i == 0){
+        	    	++count;
+        	    	temp /= i;
+        		}
+        		if(count >= 1){
+        	    	Console.Write(i);
+        	    	if(count > 1) Console.Write($"^{count}");;
+        	    	if(temp > i){
+        	        	Console.Write("*");
+        	    	}
+        		}
+    		}
+			Console.Write("\n");
+			Console.WriteLine($"n có {n.ToString().Length} chữ số ");
+  		}
+    }
+}
